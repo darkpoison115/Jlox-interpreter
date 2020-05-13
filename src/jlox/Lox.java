c63c628c604934329ad8680111f42a2ee12a4bcd
Jose Andrs Cortez <josecortez182@gmail.com>
2020-05-12 21:27:33 -0400

    chapter 7 simple interpreter

--- src/jlox/Lox.java
+++ src/jlox/Lox.java
@@ -10,7 +10,9 @@ import java.util.List;
 
 public class Lox {
 	
+	private static final Interpreter interpreter = new Interpreter();
 	static boolean hadError = false;
+	static boolean hadRuntimeError = false; 
 	
 	public static void main(String[] args) throws IOException {
 		if(args.length > 1) {
@@ -27,6 +29,7 @@ public class Lox {
 		run(new String(bytes, Charset.defaultCharset()));
 		
 		if(hadError) System.exit(65);
+		if (hadRuntimeError) System.exit(70); 
 		
 	}
 	private static void runPrompt() throws IOException {
@@ -51,7 +54,7 @@ public class Lox {
 		
 		if(hadError) return;
 		
-		System.out.println(new AstPrinter().print(expression));
+		interpreter.interpret(expression); 
 		
 		/*for(Token token : tokens) {
 			System.out.println(token);
@@ -75,5 +78,10 @@ public class Lox {
 			report(token.line, "at '" + token.lexeme + "'",message);
 		}
 	}
+    static void runtimeError(RuntimeError error) {
+    	System.err.println(error.getMessage() +     
+    				"\n[line " + error.token.line + "]");   
+	    hadRuntimeError = true;                     
+	}  
 
 }
